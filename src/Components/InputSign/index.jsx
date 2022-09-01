de7438d07124bd7bd115e83b29ded464aeff1c66
LucaCarbonel <lucacarbonelborges@gmail.com>
2022-09-01 12:22:47 -0300

    SignUp without API Conecction

--- src/Components/InputSign/index.jsx
+++ src/Components/InputSign/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.scss';
 
-const InputSign = ({value, onChange, placeholder, Icon, isRequired, type}) => {
+const InputSign = ({value, placeholder, Icon, isRequired, type, setValue}) => {
   return (
     <div className='inputBox'>
       {isRequired && <div className="inputBox__asterisk">*</div>}
       <input 
         className={isRequired ? 'inputBox__input' : 'inputBox__input2'}
-        onChange={onChange}
+        onChange={(x) => setValue(x.target.value)}
         value={value}
         placeholder={placeholder}
         type={type}
--- src/Containers/SignUp/index.jsx
+++ src/Containers/SignUp/index.jsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import './index.scss';
+
+import { useNavigate } from "react-router-dom";
+
 import Input from "./../../Components/InputSign"
+
+import { ROUTES } from './../../Constants/ROUTES';
+
 import {ReactComponent as Avatar} from './../../Assets/avatar.svg';
 import {ReactComponent as Password} from './../../Assets/password.svg';
 import {ReactComponent as At} from './../../Assets/at.svg';
@@ -9,76 +14,138 @@ import {ReactComponent as Back} from './../../Assets/back.svg';
 import {ReactComponent as Empty} from './../../Assets/empty.svg'
 import {ReactComponent as Plus} from './../../Assets/plus.svg';
 
+import './index.scss';
+
 const SignUp = () => {
+  let navigate = useNavigate();
+
   const [image, setImage] = useState(null);
   const [imageView, setImageView] = useState(null);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     setImageView(image ? URL.createObjectURL(image) : null)
   },[image])
 
+  const handlerSignUp = () => {
+    if (name && surname && email && password && confirmPassword) {
+      if(password.length < 8 && confirmPassword.length < 8 ) {
+        setError('La contraseña debe de tener mas de almenos 8 caracteres')
+        } else {
+          if(password !== confirmPassword){
+            setError('Las contraseñas no son iguales')
+          } else {
+            if ((email.search('@') === -1 && email.search('.') === -1) || email.search('@') < email.search('.') ) {
+              setError('El email no es valido')
+            } else {
+              navigate(ROUTES.home)
+            }
+          }
+        }
+    } else {
+      setError('Se deben completar todos los campos obligatorios')
+    }
+  }
+
     return (
     <div className="signUp">
-      <div className="signUp__back">
+      <div className="signUp__back" onClick={() => navigate(-1)}>
         <Back />
         Registro
       </div>
-      <div className="signUp__form">
-        <Input
-          Icon={Avatar}
-          isRequired={true}
-          placeholder="Nombre"
-        />
-        <Input
-          Icon={Avatar}
-          isRequired={true}
-          placeholder="Apellido"
-        />
-        <Input
-          Icon={At}
-          isRequired={true}
-          placeholder="Email"
-        />
-        <Input
-          Icon={Building}
-          isRequired={false}
-          placeholder="Empresa"
-        />
-        <Input
-          Icon={Password}
-          isRequired={true}
-          placeholder="Contraseña"
-        />
-        <Input
-          Icon={Password}
-          isRequired={true}
-          placeholder="ConfirmarContraseña"
-        />
-      </div>
-      <div className="signUp__box">
-        <div className="signUp__image">
-          { imageView ? 
-            (<img
-              src={imageView}
-              className="signUp__image-view"
-              alt="Foto de perfil"
-            />)
-            : (<div className="signUp__image-view" style={{paddingTop: "10px"}}>
-                <Empty className="signUp__image-empty"/>
-              </div>)
-          }
-          <div className="signUp__image-add">
-            <label
-              for="files"               
-            ><Plus /></label>
-            <input 
-              type="file"
-              onChange={(e) => setImage(e.target.files[0])}
-              // style={{visibility:'hidden'}}
-              className="signUp__image-input"
-            />
+      <div className="signUp__content">
+        <div className="signUp__form">
+          <Input
+            Icon={Avatar}
+            isRequired={true}
+            placeholder="Nombre"
+            value={name}
+            setValue={setName}
+          />
+          <Input
+            Icon={Avatar}
+            isRequired={true}
+            placeholder="Apellido"
+            value={surname}
+            setValue={setSurname}
+          />
+          <Input
+            Icon={At}
+            isRequired={true}
+            placeholder="Email"
+            value={email}
+            setValue={setEmail}
+          />
+          <Input
+            Icon={Building}
+            isRequired={false}
+            placeholder="Empresa"
+            value={company}
+            setValue={setCompany}
+          />
+          <Input
+            Icon={Password}
+            isRequired={true}
+            placeholder="Contraseña"
+            value={password}
+            setValue={setPassword}
+          />
+          <Input
+            Icon={Password}
+            isRequired={true}
+            placeholder="ConfirmarContraseña"
+            value={confirmPassword}
+            setValue={setConfirmPassword}
+          />
+        </div>
+        <div className="signUp__box">
+          <div className="signUp__image">
+            { imageView ? 
+              (<img
+                src={imageView}
+                className="signUp__image-view"
+                alt="Foto de perfil"
+              />)
+              : (<div className="signUp__image-view" style={{paddingTop: "10px"}}>
+                  <Empty className="signUp__image-empty"/>
+                </div>)
+            }
+            <div>
+              <label
+                className="signUp__image-add"
+                for="upload"    
+                style={{zIndex:2}}      
+              >
+                <div className="signUp__image-label">
+                  <Plus style={{position:'absolute'}}/>
+                </div>
+                </label>
+              <input
+                id="upload"
+                type="file"
+                onChange={(e) => setImage(e.target.files[0])}
+                style={{display:'none'}}
+                className="signUp__image-input"
+              />
+            </div>
           </div>
         </div>
+      </div>
+      {error && <div className="signUp__error">{error}</div>}
+      <div className="signUp__button-box">
+        <button className="signUp__button" onClick={() => handlerSignUp()}>
+            Crear usuario
+        </button>
+      </div>
+      <div className="signUp__redirect" onClick={() => navigate(ROUTES.signin)}>
+        Ya tienes un usuario?
+        <span style={{textDecoration: "underline"}}>Iniciar sesion</span>
       </div>
     </div>
   )
